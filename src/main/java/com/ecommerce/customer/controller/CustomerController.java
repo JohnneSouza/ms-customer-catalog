f7d447e45b44a1aefe934bf44bcccabb2700594f
JohnneSouza <jhonnesf5@gmail.com>
2020-07-20 22:58:12 -0300

    Customer Controller

---
+++ src/main/java/com/ecommerce/customer/controller/CustomerController.java
@@ -0,0 +1,42 @@
+package com.ecommerce.customer.controller;
+
+import com.ecommerce.customer.model.Customer;
+import com.ecommerce.customer.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/v1")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public Flux<Customer> findAll(){
+        return customerService.findAll();
+    }
+
+    @PostMapping
+    public Mono<Customer> save(@Valid Customer customer){
+        return customerService.save(customer);
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Customer> findCustomerById(@PathVariable String id){
+        return customerService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCustomerById(@PathVariable String id){
+        customerService.deleteCustomerById(id);
+    }
+}
--- src/main/java/com/ecommerce/customer/service/CustomerService.java
+++ src/main/java/com/ecommerce/customer/service/CustomerService.java
@@ -3,7 +3,9 @@ package com.ecommerce.customer.service;
 import com.ecommerce.customer.model.Customer;
 import com.ecommerce.customer.repository.CustomerRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -19,7 +21,10 @@ public class CustomerService {
 
     public Mono<Customer> findById(String id){
         log.info("[{}] Finding Customer", id);
-        return customerRepository.findById(id);
+        return customerRepository.findById(id)
+                .switchIfEmpty(
+                        Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found"))
+                );
     }
 
     public Mono<Customer> save(Customer customer){
